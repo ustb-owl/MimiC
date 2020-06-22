e361d7688b487f591c57765f250be9c9afeb9403
MaxXing <x@maxxsoft.net>
2020-06-22 18:17:12 +0800

    (passes) added some passes

---
+++ src/opt/passes/blkmerge.cpp
@@ -0,0 +1,115 @@
+#include <unordered_set>
+#include <memory>
+
+#include "opt/pass.h"
+#include "opt/passman.h"
+
+using namespace mimic::mid;
+using namespace mimic::opt;
+
+namespace {
+
+/*
+  merge blocks with only one jump instruction,
+  replace branch with two equal targets to jump
+
+  TODO: do not skip entry blocks
+
+  e.g.
+    %0:
+      ...                       %0:
+      jump %1                     ...
+    %1: ; preds: %0     ==>>      jump %2
+      jump %2                   %2: ; preds: %0
+    %2: ; preds: %1               ...
+      ...
+
+*/
+class BlockMerge : public FunctionPass {
+ public:
+  BlockMerge() {}
+
+  bool RunOnFunction(const UserPtr &func) override {
+    changed_ = false;
+    // traverse all basic blocks
+    for (auto it = func->begin(); it != func->end(); ++it) {
+      is_entry_ = it == func->begin();
+      it->value()->RunPass(*this);
+      if (op_ == Op::ReplaceBlock) {
+        // replace current block with target
+        it->value()->ReplaceBy(target_);
+        // remove block from current function
+        it->set_value(nullptr);
+      }
+    }
+    if (changed_) func->RemoveNull();
+    // release value in 'target'
+    target_ = nullptr;
+    return changed_;
+  }
+
+  void RunOn(BlockSSA &ssa) override {
+    // check if is jump/branch instruction
+    op_ = Op::Nop;
+    ssa.insts().back()->RunPass(*this);
+    switch (op_) {
+      case Op::IsJump: {
+        if (!is_entry_ && ssa.insts().size() == 1) {
+          op_ = Op::ReplaceBlock;
+          auto target_block = static_cast<BlockSSA *>(target_.get());
+          // get new predecessor set
+          std::unordered_set<SSAPtr> preds;
+          for (const auto &i : *target_block) {
+            if (i.value().get() != &ssa) preds.insert(i.value());
+          }
+          for (const auto &i : ssa) {
+            preds.insert(i.value());
+          }
+          // apply new predecessors to target block
+          target_block->Clear();
+          for (const auto &i : preds) {
+            target_block->AddValue(i);
+          }
+          changed_ = true;
+        }
+        break;
+      }
+      case Op::ReplaceWithJump: {
+        // create jump instruction
+        auto jump = std::make_shared<JumpSSA>(target_);
+        jump->set_logger(ssa.insts().back()->logger());
+        // replace last instruction with jump
+        ssa.insts().back() = jump;
+        changed_ = true;
+        break;
+      }
+      default:;
+    }
+  }
+
+  void RunOn(JumpSSA &ssa) override {
+    target_ = ssa[0].value();
+    op_ = Op::IsJump;
+  }
+
+  void RunOn(BranchSSA &ssa) override {
+    if (ssa[1].value() == ssa[2].value()) {
+      target_ = ssa[1].value();
+      op_ = Op::ReplaceWithJump;
+    }
+  }
+
+ private:
+  enum class Op {
+    Nop, IsJump, ReplaceBlock, ReplaceWithJump,
+  };
+
+  bool changed_, is_entry_;
+  SSAPtr target_;
+  Op op_;
+};
+
+}  // namespace
+
+// register current passs
+REGISTER_PASS(BlockMerge, block_merge, 1, false);
---
+++ src/opt/passes/dce.cpp
@@ -0,0 +1,98 @@
+#include "opt/pass.h"
+#include "opt/passman.h"
+
+using namespace mimic::mid;
+using namespace mimic::opt;
+
+namespace {
+
+// dead code elimination
+class DeadCodeEliminationPass : public FunctionPass {
+ public:
+  DeadCodeEliminationPass() {}
+
+  bool RunOnFunction(const UserPtr &func) override {
+    changed_ = false;
+    cur_func_ = func.get();
+    // traverse all basic blocks
+    for (const auto &i : *func) {
+      i.value()->RunPass(*this);
+    }
+    // rearrange uses
+    func->RemoveNull();
+    return changed_;
+  }
+
+  void RunOn(BlockSSA &ssa) override {
+    // traverse all instructions
+    for (auto it = ssa.insts().begin(); it != ssa.insts().end();) {
+      remove_flag_ = false;
+      (*it)->RunPass(*this);
+      // check if need to be removed
+      if (remove_flag_) {
+        it = ssa.insts().erase(it);
+        changed_ = true;
+      }
+      else {
+        ++it;
+      }
+    }
+    // removed non-entry blocks with no predecessors
+    if (ssa.empty() && (*cur_func_)[0].value().get() != &ssa) {
+      if (ssa.insts().size() > 1) {
+        ssa.logger()->LogWarning("unreachable code");
+      }
+      // remove current block
+      auto uses = ssa.uses();
+      ssa.ReplaceBy(nullptr);
+      // remove from all successors
+      for (const auto &i : uses) {
+        if (i->user() != cur_func_) i->user()->RemoveNull();
+      }
+      changed_ = true;
+    }
+  }
+
+  void RunOn(LoadSSA &ssa) override {
+    // NOTE: must remove all redundant loads, including volatile loads
+    if (/* !ssa.type()->IsVola() && */ ssa.uses().empty()) {
+      remove_flag_ = true;
+    }
+  }
+
+  void RunOn(AccessSSA &ssa) override {
+    if (ssa.uses().empty()) remove_flag_ = true;
+  }
+
+  void RunOn(BinarySSA &ssa) override {
+    if (ssa.uses().empty()) remove_flag_ = true;
+  }
+
+  void RunOn(UnarySSA &ssa) override {
+    if (ssa.uses().empty()) remove_flag_ = true;
+  }
+
+  void RunOn(CastSSA &ssa) override {
+    if (ssa.uses().empty()) remove_flag_ = true;
+  }
+
+  void RunOn(AllocaSSA &ssa) override {
+    if (ssa.uses().empty()) {
+      remove_flag_ = true;
+      ssa.logger()->LogWarning("unused variable definition");
+    }
+  }
+
+ private:
+  // set if IR changed
+  bool changed_;
+  // current function
+  User *cur_func_;
+  // set if need to be removed
+  bool remove_flag_;
+};
+
+}  // namespace
+
+// register current pass
+REGISTER_PASS(DeadCodeEliminationPass, dead_code_elim, 0, false);
