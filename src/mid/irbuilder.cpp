e966102dd20b505392b2238dfe49b6e866e39324
MaxXing <x@maxxsoft.net>
2020-07-06 21:08:49 +0800

    (genir) added class 'IRBuilder'

---
+++ src/mid/irbuilder.cpp
@@ -0,0 +1,138 @@
+#include "mid/irbuilder.h"
+
+using namespace mimic::mid;
+using namespace mimic::define;
+
+namespace {
+
+//
+
+}  // namespace
+
+SSAPtr IRBuilder::GenerateOn(VarDeclAST &ast) {
+  //
+}
+
+SSAPtr IRBuilder::GenerateOn(VarDefAST &ast) {
+  //
+}
+
+SSAPtr IRBuilder::GenerateOn(InitListAST &ast) {
+  //
+}
+
+SSAPtr IRBuilder::GenerateOn(FuncDeclAST &ast) {
+  //
+}
+
+SSAPtr IRBuilder::GenerateOn(FuncDefAST &ast) {
+  //
+}
+
+SSAPtr IRBuilder::GenerateOn(FuncParamAST &ast) {
+  //
+}
+
+SSAPtr IRBuilder::GenerateOn(StructDefAST &ast) {
+  //
+}
+
+SSAPtr IRBuilder::GenerateOn(EnumDefAST &ast) {
+  //
+}
+
+SSAPtr IRBuilder::GenerateOn(TypeAliasAST &ast) {
+  //
+}
+
+SSAPtr IRBuilder::GenerateOn(StructElemAST &ast) {
+  //
+}
+
+SSAPtr IRBuilder::GenerateOn(StructElemDefAST &ast) {
+  //
+}
+
+SSAPtr IRBuilder::GenerateOn(EnumElemAST &ast) {
+  //
+}
+
+SSAPtr IRBuilder::GenerateOn(BlockAST &ast) {
+  //
+}
+
+SSAPtr IRBuilder::GenerateOn(IfElseAST &ast) {
+  //
+}
+
+SSAPtr IRBuilder::GenerateOn(WhileAST &ast) {
+  //
+}
+
+SSAPtr IRBuilder::GenerateOn(ControlAST &ast) {
+  //
+}
+
+SSAPtr IRBuilder::GenerateOn(BinaryAST &ast) {
+  //
+}
+
+SSAPtr IRBuilder::GenerateOn(CastAST &ast) {
+  //
+}
+
+SSAPtr IRBuilder::GenerateOn(UnaryAST &ast) {
+  //
+}
+
+SSAPtr IRBuilder::GenerateOn(IndexAST &ast) {
+  //
+}
+
+SSAPtr IRBuilder::GenerateOn(FuncCallAST &ast) {
+  //
+}
+
+SSAPtr IRBuilder::GenerateOn(AccessAST &ast) {
+  //
+}
+
+SSAPtr IRBuilder::GenerateOn(IntAST &ast) {
+  //
+}
+
+SSAPtr IRBuilder::GenerateOn(CharAST &ast) {
+  //
+}
+
+SSAPtr IRBuilder::GenerateOn(StringAST &ast) {
+  //
+}
+
+SSAPtr IRBuilder::GenerateOn(IdAST &ast) {
+  //
+}
+
+SSAPtr IRBuilder::GenerateOn(PrimTypeAST &ast) {
+  //
+}
+
+SSAPtr IRBuilder::GenerateOn(StructTypeAST &ast) {
+  //
+}
+
+SSAPtr IRBuilder::GenerateOn(EnumTypeAST &ast) {
+  //
+}
+
+SSAPtr IRBuilder::GenerateOn(ConstTypeAST &ast) {
+  //
+}
+
+SSAPtr IRBuilder::GenerateOn(PointerTypeAST &ast) {
+  //
+}
+
+SSAPtr IRBuilder::GenerateOn(UserTypeAST &ast) {
+  //
+}
