a2994c3d3f3c8f2fbcce42aa905e35b74d748e4e
MaxXing <x@maxxsoft.net>
2020-07-10 17:02:15 +0800

    (parser) added definition of class 'Parser'

---
+++ src/front/parser.cpp
@@ -0,0 +1,180 @@
+#include "front/parser.h"
+
+#include "front/token.h"
+
+using namespace mimic::front;
+using namespace mimic::define;
+
+namespace {
+
+//
+
+}  // namespace
+
+ASTPtr Parser::LogError(std::string_view message) {
+  //
+}
+
+ASTPtr Parser::ParseCompUnit() {
+  //
+}
+
+ASTPtr Parser::ParseDecl() {
+  //
+}
+
+ASTPtr Parser::ParseTypeDef() {
+  //
+}
+
+ASTPtr Parser::ParseFuncDef() {
+  //
+}
+
+ASTPtr Parser::ParseVarDecl() {
+  //
+}
+
+ASTPtr Parser::ParseVarDef() {
+  //
+}
+
+ASTPtr Parser::ParseInitVal() {
+  //
+}
+
+ASTPtr Parser::ParseFuncDecl() {
+  //
+}
+
+ASTPtr Parser::ParseFuncHeader() {
+  //
+}
+
+ASTPtr Parser::ParseFuncParams() {
+  //
+}
+
+ASTPtr Parser::ParseFuncParam() {
+  //
+}
+
+ASTPtr Parser::ParseStructDef() {
+  //
+}
+
+ASTPtr Parser::ParseEnumDef() {
+  //
+}
+
+ASTPtr Parser::ParseTypeAlias() {
+  //
+}
+
+ASTPtr Parser::ParseStructElem() {
+  //
+}
+
+ASTPtr Parser::ParseStructElemDef() {
+  //
+}
+
+ASTPtr Parser::ParseEnumElems() {
+  //
+}
+
+ASTPtr Parser::ParseBlock() {
+  //
+}
+
+ASTPtr Parser::ParseBlockItem() {
+  //
+}
+
+ASTPtr Parser::ParseStmt() {
+  //
+}
+
+ASTPtr Parser::ParseBare() {
+  //
+}
+
+ASTPtr Parser::ParseIfElse() {
+  //
+}
+
+ASTPtr Parser::ParseWhile() {
+  //
+}
+
+ASTPtr Parser::ParseControl() {
+  //
+}
+
+ASTPtr Parser::ParseExpr() {
+  //
+}
+
+ASTPtr Parser::ParseCast() {
+  //
+}
+
+ASTPtr Parser::ParseUnary() {
+  //
+}
+
+ASTPtr Parser::ParseFactor() {
+  //
+}
+
+ASTPtr Parser::ParseValue() {
+  //
+}
+
+ASTPtr Parser::ParseIndex() {
+  //
+}
+
+ASTPtr Parser::ParseFuncCall() {
+  //
+}
+
+ASTPtr Parser::ParseAccess() {
+  //
+}
+
+ASTPtr Parser::ParseType() {
+  //
+}
+
+ASTPtr Parser::ParsePrimType() {
+  //
+}
+
+ASTPtr Parser::ParseStructType() {
+  //
+}
+
+ASTPtr Parser::ParseEnumType() {
+  //
+}
+
+ASTPtr Parser::ParseUserType() {
+  //
+}
+
+ASTPtr Parser::ParseConst() {
+  //
+}
+
+ASTPtr Parser::ParsePointer() {
+  //
+}
+
+bool Parser::ExpectChar(char c) {
+  //
+}
+
+bool Parser::ExpectId() {
+  //
+}
