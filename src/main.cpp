2c0a2a1498af41f832f04390f9c2d81d238210f1
MaxXing <x@maxxsoft.net>
2020-07-11 17:51:43 +0800

    (main) added driver code

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,23 @@
-//
+#include <iostream>
+
+#include "front/logger.h"
+#include "front/lexer.h"
+#include "front/parser.h"
+
+using namespace std;
+using namespace mimic::front;
 
 int main(int argc, const char *argv[]) {
-  //
-  return 0;
+  // get file name
+  if (argc < 2) return 1;
+  auto file = argv[1];
+  Logger::set_file(file);
+  // create lexer & parser
+  Lexer lexer(file);
+  Parser parser(lexer);
+  // get ASTs
+  while (auto ast = parser.ParseNext()) {
+    ast->Dump(std::cout);
+  }
+  return Logger::error_num();
 }
