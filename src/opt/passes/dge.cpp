53772a054c3dc8256f6cebb5b671e1fea8df9f4d
MaxXing <x@maxxsoft.net>
2020-07-16 10:42:18 +0800

    (dge) added more details

--- src/opt/passes/dge.cpp
+++ src/opt/passes/dge.cpp
@@ -6,8 +6,12 @@ using namespace mimic::opt;
 
 namespace {
 
-// dead global value elimination
-// NOTE: this pass will only remove unused function declarations
+/*
+  dead global value elimination
+  this pass will:
+  1.  remove unused function declarations
+  2.  remove unused internal/inline functions and global variables
+*/
 class DeadGlobalValEliminationPass : public ModulePass {
  public:
   DeadGlobalValEliminationPass() {}
@@ -30,11 +34,31 @@ class DeadGlobalValEliminationPass : public ModulePass {
   }
 
   void RunOn(FunctionSSA &ssa) override {
-    // remove unused declarations
-    remove_flag_ = !ssa.size() && ssa.uses().empty();
+    if (ssa.uses().empty()) {
+      // check if is internal
+      bool is_internal = IsInternal(ssa.link());
+      if (is_internal) {
+        ssa.logger()->LogWarning("unused internal function definition");
+      }
+      // mark if need to be removed
+      remove_flag_ = !ssa.size() || is_internal;
+    }
+  }
+
+  void RunOn(GlobalVarSSA &ssa) override {
+    // check if need to be removed
+    bool is_internal = IsInternal(ssa.link());
+    if (ssa.uses().empty() && is_internal) {
+      ssa.logger()->LogWarning("unused internal global variable");
+      remove_flag_ = true;
+    }
   }
 
  private:
+  bool IsInternal(LinkageTypes link) {
+    return link == LinkageTypes::Internal || link == LinkageTypes::Inline;
+  }
+
   // set if need to be removed
   bool remove_flag_;
 };
