eb3cf6742be08a1acc51e9f2b47907882914f3fe
MaxXing <x@maxxsoft.net>
2020-07-25 22:07:33 +0800

    (movelim) added MIR pass 'MoveElimination'

---
+++ src/back/asm/mir/passes/movelim.h
@@ -0,0 +1,33 @@
+#ifndef MIMIC_BACK_ASM_MIR_PASSES_MOVELIM_H_
+#define MIMIC_BACK_ASM_MIR_PASSES_MOVELIM_H_
+
+#include "back/asm/mir/pass.h"
+
+namespace mimic::back::asmgen {
+
+class MoveEliminatePass : public PassInterface {
+ public:
+  MoveEliminatePass() {}
+
+  void RunOn(InstPtrList &insts) override {
+    InstPtr last;
+    for (auto it = insts.begin(); it != insts.end();) {
+      if (last && (*it)->IsMove()) {
+        // check if current instruction needs to be removed
+        if ((*it)->oprs()[0] == last->dest()) {
+          last->set_dest((*it)->dest());
+          last = *it;
+          it = insts.erase(it);
+          continue;
+        }
+      }
+      // remember last instruction
+      last = *it;
+      ++it;
+    }
+  }
+};
+
+}  // namespace mimic::back::back::asmgen
+
+#endif  // MIMIC_BACK_ASM_MIR_PASSES_MOVELIM_H_
