220250686e2a389885460020d86b5074ced50076
MaxXing <x@maxxsoft.net>
2020-07-25 22:47:06 +0800

    (brelim, movelim) fixed some details

--- src/back/asm/arch/aarch32/passes/brelim.h
+++ src/back/asm/arch/aarch32/passes/brelim.h
@@ -12,6 +12,7 @@ class BranchEliminationPass : public PassInterface {
 
   void RunOn(InstPtrList &insts) override {
     using OpCode = AArch32Inst::OpCode;
+    // remove redundant branches
     InstPtr last;
     for (auto it = insts.begin(); it != insts.end(); ++it) {
       if (last) {
@@ -20,12 +21,24 @@ class BranchEliminationPass : public PassInterface {
         // check if last instruction can be removed
         if (last_inst->opcode() == OpCode::B &&
             cur_inst->opcode() == OpCode::LABEL &&
-            last_inst->oprs()[0] == cur_inst->oprs()[0]) {
+            last_inst->oprs()[0].value() == cur_inst->oprs()[0].value()) {
           it = insts.erase(--it);
         }
       }
       // remember last instruction
       last = *it;
+    }
+    // remove unused labels
+    for (auto it = insts.begin(); it != insts.end();) {
+      auto cur_inst = static_cast<AArch32Inst *>(it->get());
+      // check if label can be removed
+      if (cur_inst->opcode() == OpCode::LABEL &&
+          cur_inst->oprs()[0].value()->use_count() == 1) {
+        it = insts.erase(it);
+      }
+      else {
+        ++it;
+      }
     }
   }
 };
