980da293ada52636a5e25094d22b8155b871fd6f
MaxXing <x@maxxsoft.net>
2020-07-25 19:00:47 +0800

    (virtreg) added property 'size'

--- src/back/asm/mir/virtreg.h
+++ src/back/asm/mir/virtreg.h
@@ -8,7 +8,8 @@ namespace mimic::back::asmgen {
 // virtual register
 class VirtRegOperand : public OperandBase {
  public:
-  VirtRegOperand(std::uint32_t id) : id_(id) {}
+  VirtRegOperand(std::uint32_t id, std::size_t size)
+      : id_(id), size_(size) {}
 
   bool IsReg() const override { return true; }
   bool IsVirtual() const override { return true; }
@@ -21,9 +22,11 @@ class VirtRegOperand : public OperandBase {
 
   // getters
   std::uint32_t id() const { return id_; }
+  std::size_t size() const { return size_; }
 
  private:
   std::uint32_t id_;
+  std::size_t size_;
 };
 
 // virtual register factory
@@ -31,8 +34,15 @@ class VirtRegFactory {
  public:
   VirtRegFactory() : next_id_(0) {}
 
-  // get a new virtual register
-  OprPtr GetReg() { return std::make_shared<VirtRegOperand>(next_id_++); }
+  // get a new virtual register (32-bit)
+  OprPtr GetReg() {
+    return std::make_shared<VirtRegOperand>(next_id_++, 4);
+  }
+
+  // get a new virtual register (with specific size)
+  OprPtr GetReg(std::size_t size) {
+    return std::make_shared<VirtRegOperand>(next_id_++, size);
+  }
 
  private:
   std::uint32_t next_id_;
