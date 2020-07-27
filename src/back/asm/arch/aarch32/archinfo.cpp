adf4584ffabe71208427ff15eb0e0393108bdbe4
MaxXing <x@maxxsoft.net>
2020-07-27 08:42:44 +0800

    (archinfo) added some details

--- src/back/asm/arch/aarch32/archinfo.cpp
+++ src/back/asm/arch/aarch32/archinfo.cpp
@@ -3,6 +3,7 @@
 #include "back/asm/arch/aarch32/instgen.h"
 #include "back/asm/arch/aarch32/passes/brelim.h"
 #include "back/asm/mir/passes/movelim.h"
+#include "back/asm/mir/passes/fastalloc.h"
 
 using namespace mimic::back::asmgen;
 using namespace mimic::back::asmgen::aarch32;
@@ -17,9 +18,20 @@ class AArch32ArchInfo : public ArchInfoBase {
     return inst_gen_;
   }
 
-  PassPtrList GetPassList(std::size_t opt_level) const override {
+  PassPtrList GetPassList(std::size_t opt_level) override {
+    using RegName = AArch32Reg::RegName;
     PassPtrList list;
     list.push_back(MakePass<BranchEliminationPass>());
+    list.push_back(MakePass<MoveEliminatePass>());
+    // initialize register allocator
+    auto fast_alloc = MakePass<FastRegAllocPass>();
+    for (int i = static_cast<int>(RegName::R4);
+         i <= static_cast<int>(RegName::R10); ++i) {
+      fast_alloc->AddAvaliableReg(
+          inst_gen_.GetReg(static_cast<RegName>(i)));
+    }
+    fast_alloc->set_alloc_slot(inst_gen_.GetSlotAllocator());
+    list.push_back(std::move(fast_alloc));
     list.push_back(MakePass<MoveEliminatePass>());
     return list;
   }
