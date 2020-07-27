c677b9cfbe44c86b0ceed9384f7240435d2d204d
MaxXing <x@maxxsoft.net>
2020-07-27 22:04:29 +0800

    (aarch32/archinfo) updated architecture info

--- src/back/asm/arch/aarch32/archinfo.cpp
+++ src/back/asm/arch/aarch32/archinfo.cpp
@@ -2,8 +2,9 @@
 
 #include "back/asm/arch/aarch32/instgen.h"
 #include "back/asm/arch/aarch32/passes/brelim.h"
+#include "back/asm/mir/passes/movprop.h"
 #include "back/asm/mir/passes/movelim.h"
-#include "back/asm/mir/passes/fastalloc.h"
+#include "back/asm/mir/passes/linearscan.h"
 
 using namespace mimic::back::asmgen;
 using namespace mimic::back::asmgen::aarch32;
@@ -14,6 +15,8 @@ class AArch32ArchInfo : public ArchInfoBase {
  public:
   AArch32ArchInfo() {}
 
+  std::size_t GetPtrSize() const override { return 4; }
+
   InstGenBase &GetInstGen() override {
     return inst_gen_;
   }
@@ -22,17 +25,17 @@ class AArch32ArchInfo : public ArchInfoBase {
     using RegName = AArch32Reg::RegName;
     PassPtrList list;
     list.push_back(MakePass<BranchEliminationPass>());
+    list.push_back(MakePass<MovePropagationPass>());
     list.push_back(MakePass<MoveEliminatePass>());
     // initialize register allocator
-    auto fast_alloc = MakePass<FastRegAllocPass>();
+    auto reg_alloc = MakePass<LinearScanRegAllocPass>();
     for (int i = static_cast<int>(RegName::R4);
          i <= static_cast<int>(RegName::R10); ++i) {
-      fast_alloc->AddAvaliableReg(
+      reg_alloc->AddAvaliableReg(
           inst_gen_.GetReg(static_cast<RegName>(i)));
     }
-    fast_alloc->set_slot_alloc(inst_gen_.GetSlotAllocator());
-    list.push_back(std::move(fast_alloc));
-    list.push_back(MakePass<MoveEliminatePass>());
+    reg_alloc->set_slot_alloc(inst_gen_.GetSlotAllocator());
+    list.push_back(std::move(reg_alloc));
     return list;
   }
 
