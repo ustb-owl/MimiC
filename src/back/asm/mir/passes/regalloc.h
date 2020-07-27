609376cb02a348fc67f1ec3b62411891ebdc9aef
MaxXing <x@maxxsoft.net>
2020-07-27 21:28:37 +0800

    (regalloc, fastalloc) updated interface

--- src/back/asm/mir/passes/regalloc.h
+++ src/back/asm/mir/passes/regalloc.h
@@ -2,6 +2,8 @@
 #define MIMIC_BACK_ASM_MIR_PASSES_REGALLOC_H_
 
 #include <functional>
+#include <unordered_set>
+#include <cassert>
 
 #include "back/asm/mir/pass.h"
 
@@ -20,12 +22,23 @@ class RegAllocatorBase : public PassInterface {
     slot_alloc_ = slot_alloc;
   }
 
+  // getters
+  const std::unordered_set<OprPtr> &allocated_regs() const {
+    return allocated_regs_;
+  }
+
  protected:
   // allocate a new slot
   OprPtr AllocSlot() const { return slot_alloc_(); }
+  // add allocated register to set
+  void MarkAsAllocated(const OprPtr &reg) {
+    assert(reg->IsReg());
+    allocated_regs_.insert(reg);
+  }
 
  private:
   std::function<OprPtr()> slot_alloc_;
+  std::unordered_set<OprPtr> allocated_regs_;
 };
 
 }  // namespace mimic::back::asmgen
