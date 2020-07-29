3f5727a2c4db3042ccfb1f80d409e5ba8b22908a
MaxXing <x@maxxsoft.net>
2020-07-29 10:19:01 +0800

    (fastalloc) fixed some faults

--- src/back/asm/mir/passes/fastalloc.h
+++ src/back/asm/mir/passes/fastalloc.h
@@ -1,9 +1,9 @@
 #ifndef MIMIC_BACK_ASM_MIR_PASSES_FASTALLOC_H_
 #define MIMIC_BACK_ASM_MIR_PASSES_FASTALLOC_H_
 
+#include <list>
 #include <queue>
 #include <unordered_map>
-#include <functional>
 #include <cassert>
 
 #include "back/asm/mir/passes/regalloc.h"
@@ -12,71 +12,64 @@ namespace mimic::back::asmgen {
 
 /*
   fast register allocation
-
-  TODO: this algorithm is wrong when processing loops
+  allocate register if there are remaining registers
+  otherwise, allocate a new slot
 */
 class FastRegAllocPass : public RegAllocatorBase {
  public:
   FastRegAllocPass() {}
 
   void RunOn(const OprPtr &func_label, InstPtrList &insts) override {
+    Reset();
     for (const auto &i : insts) {
-      // retire operands
       for (auto &&use : i->oprs()) {
-        // get virtual register info
-        auto opr = use.value();
-        if (!opr->IsVirtual()) continue;
-        auto it = allocated_vregs_.find(opr);
-        assert(it != allocated_vregs_.end());
-        // replace with allocated operand
-        use.set_value(it->second);
-        if (!opr->use_count()) {
-          // current virtual register is inactive
-          if (it->second->IsReg()) {
-            unused_regs_.push(it->second);
-          }
-          else {
-            assert(it->second->IsSlot());
-            unused_slots_.push(it->second);
-          }
+        if (use.value()->IsVirtual()) {
+          use.set_value(Allocate(use.value(), func_label));
         }
       }
-      // check destination operand
-      if (!i->dest() || !i->dest()->IsVirtual()) continue;
-      OprPtr opr;
-      auto it = allocated_vregs_.find(i->dest());
-      if (it != allocated_vregs_.end()) {
-        // use allocated register
-        opr = it->second;
+      if (i->dest() && i->dest()->IsVirtual()) {
+        i->set_dest(Allocate(i->dest(), func_label));
       }
-      else {
-        // allocate destination operand
-        if (!unused_regs_.empty()) {
-          opr = unused_regs_.front();
-          unused_regs_.pop();
-        }
-        else if (!unused_slots_.empty()) {
-          opr = unused_slots_.front();
-          unused_slots_.pop();
-        }
-        else {
-          opr = allocator().AllocateSlot(func_label);
-        }
-        // mark virtual register as allocated
-        allocated_vregs_[i->dest()] = opr;
-      }
-      i->set_dest(opr);
     }
   }
 
-  // add avaliable architecture
   void AddAvaliableReg(const OprPtr &reg) override {
-    unused_slots_.push(reg);
+    avaliable_regs_.push_back(reg);
   }
 
  private:
-  // unused architecture regsters & retired stack slots
-  std::queue<OprPtr> unused_regs_, unused_slots_;
+  void Reset() {
+    while (!unused_regs_.empty()) unused_regs_.pop();
+    for (const auto &i : avaliable_regs_) unused_regs_.push(i);
+    allocated_vregs_.clear();
+  }
+
+  const OprPtr &Allocate(const OprPtr &vreg, const OprPtr &func_label) {
+    assert(vreg->IsVirtual());
+    // find in allocated virtual registers
+    auto it = allocated_vregs_.find(vreg);
+    if (it != allocated_vregs_.end()) {
+      return it->second;
+    }
+    else {
+      OprPtr pos;
+      // try to allocate a register
+      if (!unused_regs_.empty()) {
+        pos = unused_regs_.front();
+        unused_regs_.pop();
+      }
+      else {
+        // allocate a new slot
+        pos = allocator().AllocateSlot(func_label);
+      }
+      return allocated_vregs_.insert({vreg, pos}).first->second;
+    }
+  }
+
+  // all avaliable architecture registers
+  std::list<OprPtr> avaliable_regs_;
+  // unused architecture regsters
+  std::queue<OprPtr> unused_regs_;
   // allocated virtual registers
   std::unordered_map<OprPtr, OprPtr> allocated_vregs_;
 };
