692dfcfbe55b41c423b44145f1b337f134116294
MaxXing <x@maxxsoft.net>
2020-08-03 23:40:30 +0800

    (const) added helper class for const folding

---
+++ src/opt/passes/helper/const.h
@@ -0,0 +1,43 @@
+#ifndef MIMIC_OPT_PASSES_HELPER_CONST_H_
+#define MIMIC_OPT_PASSES_HELPER_CONST_H_
+
+#include <memory>
+
+#include "mid/ssa.h"
+
+namespace mimic::opt {
+
+class ConstantHelper {
+ public:
+  using ConstIntPtr = std::shared_ptr<mid::ConstIntSSA>;
+
+  // fold constant instruction with int/ptr type if possible
+  // returns nullptr if failed
+  static ConstIntPtr Fold(const mid::SSAPtr &val);
+
+  // fold binary operation if possible
+  // lhs & rhs must be constant
+  static ConstIntPtr Fold(mid::BinarySSA::Operator op,
+                          const mid::SSAPtr &lhs, const mid::SSAPtr &rhs);
+
+  // fold unary operation if possible
+  static ConstIntPtr Fold(mid::UnarySSA::Operator op,
+                          const mid::SSAPtr &opr);
+
+  // fold type casting if possible
+  static ConstIntPtr Fold(const mid::SSAPtr &opr,
+                          const define::TypePtr &dest_ty);
+
+  // check if two values are identical
+  // if values are both constant, compare their actual values
+  // otherwise, perform pointer comparison
+  static bool IsIdentical(const mid::SSAPtr &val1, const mid::SSAPtr &val2);
+
+ private:
+  ConstantHelper() {}
+};
+
+
+}  // namespace mimic::opt
+
+#endif  // MIMIC_OPT_PASSES_HELPER_CONST_H_
