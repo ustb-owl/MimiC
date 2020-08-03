d6646063619eb680630572a8094fb1bba30776c7
MaxXing <x@maxxsoft.net>
2020-08-03 20:06:20 +0800

    (licm) fixed some bugs

--- src/opt/passes/licm.cpp
+++ src/opt/passes/licm.cpp
@@ -26,32 +26,26 @@ class LoopInvariantCodeMotionPass : public FunctionPass {
     auto func_ptr = SSACast<FunctionSSA>(func.get());
     if (func_ptr->is_decl()) return false;
     // run on loops
-    bool re_run = true, changed = false;
-    while (re_run) {
-      re_run = false;
-      // prepare parent info
-      // must be rescanned in each iteration since parent info
-      // may be changed due to preheader insertion
-      ParentScanner parent(func_ptr);
-      parent_ = &parent;
-      // prepare dominance checker
-      DominanceChecker dom(func_ptr);
-      dom_ = &dom;
-      // scan for all loops
-      LoopDetector loop_det(dom, func_ptr);
-      for (const auto &info : loop_det.loops()) {
-        cur_loop_ = &info;
-        if (ProcessLoop()) {
-          re_run = true;
-          changed = true;
-        }
-      }
+    bool changed = false;
+    // prepare parent info
+    // must be rescanned in each iteration since parent info
+    // may be changed due to preheader insertion
+    ParentScanner parent(func_ptr);
+    parent_ = &parent;
+    // prepare dominance checker
+    DominanceChecker dom(func_ptr);
+    dom_ = &dom;
+    // scan for all loops
+    LoopDetector loop_det(dom, func_ptr);
+    for (const auto &info : loop_det.loops()) {
+      cur_loop_ = &info;
+      if (ProcessLoop()) changed = true;
     }
-    assert(invs_.empty());
+    // clean up
+    invs_.clear();
     return changed;
   }
 
-  void RunOn(LoadSSA &ssa) override { LogInvariant(ssa); }
   void RunOn(AccessSSA &ssa) override { LogInvariant(ssa); }
   void RunOn(BinarySSA &ssa) override { LogInvariant(ssa); }
   void RunOn(UnarySSA &ssa) override { LogInvariant(ssa); }
@@ -59,9 +53,18 @@ class LoopInvariantCodeMotionPass : public FunctionPass {
   void RunOn(AllocaSSA &ssa) override { assert(false); }
   void RunOn(SelectSSA &ssa) override { LogInvariant(ssa); }
 
+  // special treatment for load instructions
+  // do not mark loads with pointer that has been stored in loop
+  void RunOn(LoadSSA &ssa) override {
+    if (stored_ptrs_.count(GetBasePointer(ssa.ptr().get()))) return;
+    LogInvariant(ssa);
+  }
+
  private:
   bool IsInvariant(const SSAPtr &val);
   void LogInvariant(User &ssa);
+  Value *GetBasePointer(Value *ptr);
+  void ProcessStores();
   bool ProcessLoop();
 
   // helper passes
@@ -74,6 +77,8 @@ class LoopInvariantCodeMotionPass : public FunctionPass {
   // all invariants in current loop
   std::unordered_set<Value *> marked_invs_;
   SSAPtrList invs_;
+  // pointers that has been stored in current loop
+  std::unordered_set<Value *> stored_ptrs_;
 };
 
 }  // namespace
@@ -114,7 +119,57 @@ void LoopInvariantCodeMotionPass::LogInvariant(User &ssa) {
   }
 }
 
+Value *LoopInvariantCodeMotionPass::GetBasePointer(Value *ptr) {
+  // get base pointer of access/cast
+  for (;;) {
+    if (auto acc = SSADynCast<AccessSSA>(ptr)) {
+      ptr = acc->ptr().get();
+    }
+    else if (auto cast = SSADynCast<CastSSA>(ptr)) {
+      ptr = cast->opr().get();
+    }
+    else {
+      return ptr;
+    }
+  }
+}
+
+void LoopInvariantCodeMotionPass::ProcessStores() {
+  // TODO: actually there should be a pointer alias analysis here
+  stored_ptrs_.clear();
+  for (const auto &block : cur_loop_->body) {
+    for (const auto &i : block->insts()) {
+      if (auto store = SSADynCast<StoreSSA>(i.get())) {
+        auto ptr = store->ptr().get();
+        // get base pointer of access/cast
+        for (;;) {
+          if (auto acc = SSADynCast<AccessSSA>(ptr)) {
+            ptr = acc->ptr().get();
+          }
+          else if (auto cast = SSADynCast<CastSSA>(ptr)) {
+            ptr = cast->opr().get();
+          }
+          else {
+            break;
+          }
+        }
+        // if stored to argument, treat all arguments as stored
+        // to prevent pointer alias
+        if (IsSSA<ArgRefSSA>(ptr)) {
+          auto &args = SSACast<FunctionSSA>(block->parent().get())->args();
+          for (const auto &arg : args) {
+            if (arg->type()->IsPointer()) stored_ptrs_.insert(arg.get());
+          }
+        }
+        stored_ptrs_.insert(GetBasePointer(ptr));
+      }
+    }
+  }
+}
+
 bool LoopInvariantCodeMotionPass::ProcessLoop() {
+  // mark all stored pointers in loop
+  ProcessStores();
   // scan all invariants
   marked_invs_.clear();
   invs_.clear();
