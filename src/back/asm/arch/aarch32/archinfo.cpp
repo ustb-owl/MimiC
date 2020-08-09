2a4c7b5ffccd9a32db48e2b48cfeddef29d5f73c
MaxXing <x@maxxsoft.net>
2020-08-09 16:19:32 +0800

    (aarch32/archinfo) updated architecture info

--- src/back/asm/arch/aarch32/archinfo.cpp
+++ src/back/asm/arch/aarch32/archinfo.cpp
@@ -36,10 +36,10 @@ class AArch32ArchInfo : public ArchInfoBase {
       list.push_back(MakePass<MoveEliminatePass>());
     }
     InitRegAlloc(opt_level, list);
+    list.push_back(MakePass<SlotSpillingPass>(inst_gen_));
     if (opt_level) {
       list.push_back(MakePass<LoadStorePropagationPass>());
     }
-    list.push_back(MakePass<SlotSpillingPass>(inst_gen_));
     list.push_back(MakePass<FuncDecoratePass>(inst_gen_));
     list.push_back(MakePass<ImmNormalizePass>(inst_gen_));
     return list;
