3268d797e72b82082506da23fb732809b3f8d2d2
MaxXing <x@maxxsoft.net>
2020-08-13 17:55:36 +0800

    (regalloc) updated interface of register allocator

--- src/back/asm/mir/passes/regalloc.h
+++ src/back/asm/mir/passes/regalloc.h
@@ -3,8 +3,10 @@
 
 #include <map>
 #include <unordered_map>
-#include <functional>
 #include <unordered_set>
+#include <vector>
+#include <functional>
+#include <memory>
 #include <cassert>
 
 #include "back/asm/mir/pass.h"
@@ -47,6 +49,12 @@ using IfGraph = std::map<OprPtr, IfGraphNodeInfo, NodeCompare>;
 // interference graph of all functions
 using FuncIfGraphs = std::unordered_map<OprPtr, IfGraph>;
 
+// avaliable register list
+using RegList = std::vector<OprPtr>;
+
+// avaliable register list reference of all functions
+using FuncRegList = std::unordered_map<OprPtr, const RegList *>;
+
 // type of temporary register checker
 using TempRegChecker = std::function<bool(const OprPtr &)>;
 
@@ -75,12 +83,15 @@ class RegAllocatorBase : public PassInterface {
  public:
   virtual ~RegAllocatorBase() = default;
 
-  // add avaliable architecture temporary register
-  virtual void AddAvaliableTempReg(const OprPtr &reg) = 0;
-  // add avaliable architecture register
-  virtual void AddAvaliableReg(const OprPtr &reg) = 0;
-
   // setters
+  // specify reference of avaliable temporary architecture register list
+  void set_func_temp_reg_list(const FuncRegList *func_temp_reg_list) {
+    func_temp_reg_list_ = func_temp_reg_list;
+  }
+  // specify reference of avaliable architecture register list
+  void set_func_reg_list(const FuncRegList *func_reg_list) {
+    func_reg_list_ = func_reg_list;
+  }
   // specify stack slot allocator
   void set_allocator(SlotAllocator allocator) { allocator_ = allocator; }
   // specify temporary register checker
@@ -89,6 +100,23 @@ class RegAllocatorBase : public PassInterface {
   }
 
  protected:
+  // get avaliable temporary register list of the specific function
+  const RegList &GetTempRegList(const OprPtr &func_label) const {
+    auto it = func_temp_reg_list_->find(func_label);
+    assert(it != func_temp_reg_list_->end());
+    return *it->second;
+  }
+
+  // get avaliable register list of the specific function
+  const RegList &GetRegList(const OprPtr &func_label) const {
+    auto it = func_reg_list_->find(func_label);
+    assert(it != func_reg_list_->end());
+    return *it->second;
+  }
+
+  // check if the specific operand is a temporary register
+  bool IsTempReg(const OprPtr &opr) const { return temp_checker_(opr); }
+
   // specify 'alloc_to' property of the specific
   void AllocateVRegTo(const OprPtr &vreg, const OprPtr &dest) {
     assert(vreg->IsVirtual() &&
@@ -96,16 +124,17 @@ class RegAllocatorBase : public PassInterface {
     static_cast<VirtRegOperand *>(vreg.get())->set_alloc_to(dest);
   }
 
-  // check if the specific operand is a temporary register
-  bool IsTempReg(const OprPtr &opr) const { return temp_checker_(opr); }
-
   // getters
   const SlotAllocator &allocator() const { return allocator_; }
 
  private:
+  const FuncRegList *func_temp_reg_list_, *func_reg_list_;
   SlotAllocator allocator_;
   TempRegChecker temp_checker_;
 };
+
+// pointer to register allocator
+using RegAllocPtr = std::unique_ptr<RegAllocatorBase>;
 
 }  // namespace mimic::back::asmgen
 
