21a5119985161e551e2e77b14c9b21b8890cb3b3
MaxXing <x@maxxsoft.net>
2020-10-02 16:03:42 +0800

    (riscv32/instdef) added some definitions

---
+++ src/back/asm/arch/riscv32/instdef.cpp
@@ -0,0 +1,99 @@
+#include "back/asm/arch/riscv32/instdef.h"
+
+#include "utils/strprint.h"
+
+using namespace mimic::back::asmgen;
+using namespace mimic::back::asmgen::riscv32;
+
+namespace {
+
+const char *kRegNames[] = {
+  "zero", "ra", "sp", "x3", "x4", "t0", "t1", "t2", "fp", "s1",
+  "a0", "a1", "a2", "a3", "a4", "a5", "a6", "a7",
+  "s2", "s3", "s4", "s5", "s6", "s7", "s8", "s9", "s10", "s11",
+  "t3", "t4", "t5", "t6",
+};
+
+const char *kOpCodes[] = {
+  "lw", "lb", "lbu", "sw", "sb",
+  "addi", "slti", "sltiu", "add", "sub", "slt", "sltu",
+  "mul", "div", "divu", "rem", "remu",
+  "neg",
+  "seqz",
+  "call", "ret", "j",
+  "blt", "ble", "bgt", "bge", "bltu", "bleu", "bgtu", "bgeu", "beqz",
+  "la", "li", "mv",
+  "xori", "ori", "andi", "xor", "or", "and",
+  "not",
+  "slli", "srli", "srai", "sll", "srl", "sra",
+  "",
+  "lea", "br",
+  "seteq", "setne",
+  "setult", "setslt", "setule", "setsle",
+  "setugt", "setsgt", "setuge", "setsge",
+  ".zero", ".asciz", ".long", ".byte",
+};
+
+std::ostream &operator<<(std::ostream &os, RISCV32Reg::RegName name) {
+  os << kRegNames[static_cast<int>(name)];
+  return os;
+}
+
+std::ostream &operator<<(std::ostream &os, RISCV32Inst::OpCode opcode) {
+  os << kOpCodes[static_cast<int>(opcode)];
+  return os;
+}
+
+std::ostream &operator<<(std::ostream &os, const OprPtr &opr) {
+  opr->Dump(os);
+  return os;
+}
+
+std::ostream &operator<<(std::ostream &os, const std::vector<Use> &oprs) {
+  for (std::size_t i = 0; i < oprs.size(); ++i) {
+    if (i) os << ", ";
+    oprs[i].value()->Dump(os);
+  }
+  return os;
+}
+
+}  // namespace
+
+void RISCV32Reg::Dump(std::ostream &os) const {
+  os << name_;
+}
+
+void RISCV32Imm::Dump(std::ostream &os) const {
+  os << val_;
+}
+
+void RISCV32Int::Dump(std::ostream &os) const {
+  os << val_;
+}
+
+void RISCV32Str::Dump(std::ostream &os) const {
+  os << '"';
+  utils::DumpStr(os, str_);
+  os << '"';
+}
+
+void RISCV32Slot::Dump(std::ostream &os) const {
+  os << offset_ << '(' << base_ << ')';
+}
+
+void RISCV32Inst::Dump(std::ostream &os) const {
+  using OpCode = RISCV32Inst::OpCode;
+  if (opcode_ == OpCode::LABEL) {
+    os << oprs()[0].value() << ':';
+  }
+  else {
+    os << '\t' << opcode_ << '\t';
+    if (!dest() && !oprs().empty()) {
+      os << oprs();
+    }
+    else if (dest()) {
+      os << dest() << ", " << oprs();
+    }
+  }
+  os << std::endl;
+}
---
+++ src/back/asm/arch/riscv32/instdef.h
@@ -0,0 +1,227 @@
+#ifndef MIMIC_BACK_ASM_ARCH_RISCV32_INSTDEF_H_
+#define MIMIC_BACK_ASM_ARCH_RISCV32_INSTDEF_H_
+
+#include <string>
+#include <cstdint>
+#include <cassert>
+
+#include "back/asm/mir/mir.h"
+
+namespace mimic::back::asmgen::riscv32 {
+
+// riscv32 register
+class RISCV32Reg : public OperandBase {
+ public:
+  enum class RegName {
+    // hard-wired zero
+    Zero,
+    // return address
+    RA,
+    // stack pointer
+    SP,
+    // global pointer & thread pointer
+    X3, X4,
+    // temporaries
+    T0, T1, T2,
+    // frame pointer
+    FP,
+    // saved register
+    S1,
+    // arguments/return values
+    A0, A1,
+    // arguments
+    A2, A3, A4, A5, A6, A7,
+    // saved registers
+    S2, S3, S4, S5, S6, S7, S8, S9, S10, S11,
+    // temporaries
+    T3, T4, T5, T6,
+  };
+
+  RISCV32Reg(RegName name) : name_(name) {}
+
+  bool IsReg() const override { return true; }
+  bool IsVirtual() const override { return false; }
+  bool IsImm() const override { return false; }
+  bool IsLabel() const override { return false; }
+  bool IsSlot() const override { return false; }
+
+  void Dump(std::ostream &os) const override;
+
+  // getters
+  RegName name() const { return name_; }
+
+ private:
+  RegName name_;
+};
+
+// riscv32 immediate number
+class RISCV32Imm : public OperandBase {
+ public:
+  RISCV32Imm(std::int32_t val) : val_(val) {}
+
+  bool IsReg() const override { return false; }
+  bool IsVirtual() const override { return false; }
+  bool IsImm() const override { return true; }
+  bool IsLabel() const override { return false; }
+  bool IsSlot() const override { return false; }
+
+  void Dump(std::ostream &os) const override;
+
+  // getters
+  std::int32_t val() const { return val_; }
+
+ private:
+  std::int32_t val_;
+};
+
+// riscv32 integer (used in directives)
+class RISCV32Int : public OperandBase {
+ public:
+  RISCV32Int(std::int32_t val) : val_(val) {}
+
+  bool IsReg() const override { return false; }
+  bool IsVirtual() const override { return false; }
+  bool IsImm() const override { return false; }
+  bool IsLabel() const override { return false; }
+  bool IsSlot() const override { return false; }
+
+  void Dump(std::ostream &os) const override;
+
+  // getters
+  std::int32_t val() const { return val_; }
+
+ private:
+  std::int32_t val_;
+};
+
+// riscv32 string (used in directives)
+class RISCV32Str : public OperandBase {
+ public:
+  RISCV32Str(const std::string &str) : str_(str) {}
+
+  bool IsReg() const override { return false; }
+  bool IsVirtual() const override { return false; }
+  bool IsImm() const override { return false; }
+  bool IsLabel() const override { return false; }
+  bool IsSlot() const override { return false; }
+
+  void Dump(std::ostream &os) const override;
+
+  // getters
+  const std::string &str() const { return str_; }
+
+ private:
+  std::string str_;
+};
+
+// riscv32 slot
+class RISCV32Slot : public OperandBase {
+ public:
+  RISCV32Slot(const OprPtr &base, std::int32_t offset)
+      : base_(base), offset_(offset) {
+    assert(base->IsReg());
+  }
+
+  bool IsReg() const override { return false; }
+  bool IsVirtual() const override { return false; }
+  bool IsImm() const override { return false; }
+  bool IsLabel() const override { return false; }
+  bool IsSlot() const override { return true; }
+
+  void Dump(std::ostream &os) const override;
+
+  // getters
+  const OprPtr &base() const { return base_; }
+  std::int32_t offset() const { return offset_; }
+
+ private:
+  OprPtr base_;
+  std::int32_t offset_;
+};
+
+// riscv32 instruction
+class RISCV32Inst : public InstBase {
+ public:
+  enum class OpCode {
+    // memory accessing
+    LW, LB, LBU, SW, SB,
+    // arithmetic
+    ADDI, SLTI, SLTIU, ADD, SUB, SLT, SLTU,
+    MUL, DIV, DIVU, REM, REMU,
+    NEG,
+    // comparison
+    SEQZ,
+    // branch/jump
+    CALL, RET, J,
+    BLT, BLE, BGT, BGE, BLTU, BLEU, BGTU, BGEU, BEQZ,
+    // data processing
+    LA, LI, MV,
+    // logical
+    XORI, ORI, ANDI, XOR, OR, AND,
+    NOT,
+    // shifting
+    SLLI, SRLI, SRAI, SLL, SRL, SRA,
+    // just a label definition
+    LABEL,
+    // pseudo instructions
+    LEA, BR,
+    SETEQ, SETNE,
+    SETULT, SETSLT, SETULE, SETSLE,
+    SETUGT, SETSGT, SETUGE, SETSGE,
+    // assembler directives
+    ZERO, ASCIZ, LONG, BYTE,
+  };
+
+  // add/sub/...
+  RISCV32Inst(OpCode opcode, const OprPtr &dest, const OprPtr &opr1,
+              const OprPtr &opr2)
+      : opcode_(opcode) {
+    if (opcode == OpCode::BR) {
+      set_dest(nullptr);
+      AddOpr(dest);
+    }
+    else {
+      set_dest(dest);
+    }
+    AddOpr(opr1);
+    AddOpr(opr2);
+  }
+  // lw/mv/...
+  RISCV32Inst(OpCode opcode, const OprPtr &dest, const OprPtr &opr)
+      : opcode_(opcode) {
+    if (opcode_ == OpCode::SW || opcode_ == OpCode::SB) {
+      // SW/SB does not have destination register
+      set_dest(nullptr);
+      AddOpr(dest);
+    }
+    else {
+      set_dest(dest);
+    }
+    AddOpr(opr);
+  }
+  // label/...
+  RISCV32Inst(OpCode opcode, const OprPtr &opr)
+      : opcode_(opcode) {
+    set_dest(nullptr);
+    AddOpr(opr);
+  }
+  // ret/...
+  RISCV32Inst(OpCode opcode) : opcode_(opcode) {
+    set_dest(nullptr);
+  }
+
+  bool IsMove() const override { return opcode_ == OpCode::MV; }
+  bool IsLabel() const override { return opcode_ == OpCode::LABEL; }
+  bool IsCall() const override { return opcode_ == OpCode::CALL; }
+  void Dump(std::ostream &os) const override;
+
+  // getters
+  OpCode opcode() const { return opcode_; }
+
+ private:
+  OpCode opcode_;
+};
+
+}  // namespace mimic::back::asmgen::riscv32
+
+#endif  // MIMIC_BACK_ASM_ARCH_RISCV32_INSTDEF_H_
